fd335febb62f2ee324676a17c6372de20caa6c00
brett.chen <fox5645x@gmail.com>
2021-03-21 13:54:01 +0800

    [add]建立基本專案架構

---
+++ database/migrations/2021_03_21_050603_product.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Product extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // 產品資料表
+        Schema::create('product', function (Blueprint $table) {
+            $table->bigIncrements('id');                            // 唯一編號
+            $table->char('uuid');                                   // UUID
+            $table->unsignedInteger('product_type_id')->length(10); // 產品類別唯一編號
+            $table->string('name')->length(50);                     // 產品名稱
+            $table->text('description')->length(200)->nullable();   // 備註
+            $table->tinyInteger('is_delete')->length(1);            // 是否刪除
+            $table->unsignedBigInteger('create_by')->length(20);    // 建立者
+            $table->dateTime('create_time');                        // 建立時間
+            $table->unsignedBigInteger('modify_by')->length(20);    // 修改者
+            $table->dateTime('modify_time');                        // 修改時間
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
---
+++ database/migrations/2021_03_21_053759_product_type.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ProductType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // 產品類型資料表
+        Schema::create('product_type', function (Blueprint $table) {
+            $table->bigIncrements('id');                            // 唯一編號
+            $table->char('uuid');                                   // UUID
+            $table->string('name')->length(20);                     // 產品類型名稱
+            $table->tinyInteger('is_delete')->length(1);            // 是否刪除
+            $table->unsignedBigInteger('create_by')->length(20);    // 建立者
+            $table->dateTime('create_time');                        // 建立時間
+            $table->unsignedBigInteger('modify_by')->length(20);    // 修改者
+            $table->dateTime('modify_time');                        // 修改時間
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_type');
+    }
+}
