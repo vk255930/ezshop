8832ecbfd408a1dd40bbb671b5a7b3d80d902f9a
brett.chen <fox5645x@gmail.com>
2021-03-21 14:58:05 +0800

    [mod]主頁，程式碼樣板編排調整

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('product');
+});
+
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
