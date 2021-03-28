77735af259e128a1ac77c714203f6fd135d937c9
brett.chen <fox5645x@gmail.com>
2021-03-28 18:27:44 +0800

    [mod]產品類型、產品，更新資料表

--- database/migrations/2021_03_21_050603_product.php
+++ database/migrations/2021_03_21_050603_product.php
@@ -15,16 +15,18 @@ class Product extends Migration
     {
         // 產品資料表
         Schema::create('product', function (Blueprint $table) {
-            $table->bigIncrements('id');                            // 唯一編號
-            $table->char('uuid');                                   // UUID
-            $table->unsignedInteger('product_type_id')->length(10); // 產品類別唯一編號
-            $table->string('name')->length(50);                     // 產品名稱
-            $table->text('description')->length(200)->nullable();   // 備註
-            $table->tinyInteger('is_delete')->length(1);            // 是否刪除
-            $table->unsignedBigInteger('create_by')->length(20);    // 建立者
-            $table->dateTime('create_time');                        // 建立時間
-            $table->unsignedBigInteger('modify_by')->length(20);    // 修改者
-            $table->dateTime('modify_time');                        // 修改時間
+            $table->bigIncrements('id');                                // 唯一編號
+            $table->char('uuid');                                       // UUID
+            $table->unsignedInteger('product_type_id')->length(10);     // 產品類別唯一編號
+            $table->string('name')->length(50);                         // 產品名稱
+            $table->decimal('amount', 12, 2);                           // 產品金額
+            $table->text('img_path')->text(200)->nullable();            // 照片
+            $table->text('description')->length(200)->nullable();       // 備註
+            $table->tinyInteger('is_delete')->length(1)->default(0);    // 是否刪除
+            $table->unsignedBigInteger('create_by')->length(20);        // 建立者
+            $table->dateTime('create_time');                            // 建立時間
+            $table->unsignedBigInteger('modify_by')->length(20);        // 修改者
+            $table->dateTime('modify_time');                            // 修改時間
         });
     }
 
--- database/migrations/2021_03_21_053759_product_type.php
+++ database/migrations/2021_03_21_053759_product_type.php
@@ -15,14 +15,14 @@ class ProductType extends Migration
     {
         // 產品類型資料表
         Schema::create('product_type', function (Blueprint $table) {
-            $table->bigIncrements('id');                            // 唯一編號
-            $table->char('uuid');                                   // UUID
-            $table->string('name')->length(20);                     // 產品類型名稱
-            $table->tinyInteger('is_delete')->length(1);            // 是否刪除
-            $table->unsignedBigInteger('create_by')->length(20);    // 建立者
-            $table->dateTime('create_time');                        // 建立時間
-            $table->unsignedBigInteger('modify_by')->length(20);    // 修改者
-            $table->dateTime('modify_time');                        // 修改時間
+            $table->bigIncrements('id');                                // 唯一編號
+            $table->char('uuid');                                       // UUID
+            $table->string('name')->length(20);                         // 產品類型名稱
+            $table->tinyInteger('is_delete')->length(1)->default(0);    // 是否刪除
+            $table->unsignedBigInteger('create_by')->length(20);        // 建立者
+            $table->dateTime('create_time');                            // 建立時間
+            $table->unsignedBigInteger('modify_by')->length(20);        // 修改者
+            $table->dateTime('modify_time');                            // 修改時間
         });
     }
 
